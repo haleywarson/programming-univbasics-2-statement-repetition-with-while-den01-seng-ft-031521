74ec8b80568815d196eaf8305c62d10e7928bd65
Haley Warson <haleywarson@gmail.com>
2021-01-30 17:12:53 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,7 +1,8 @@
 
-count = 0 
-while count <=10 && count >1 do
-  count += 1
+count = 10
+while count >=1 do
+  puts count
+  count -= 1
 end
-puts "Happy New Year"
+puts "Happy New Year!"
 # Write your code here
